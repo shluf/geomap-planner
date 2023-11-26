114b0b562c38996443ba2bd318fbf9dedbcee9e1
NaufalAfrizal55 <naufalafrizal02@gmail.com>
2023-11-26 20:14:37 +0700

    add delete function

--- app/mission/page.jsx
+++ app/mission/page.jsx
@@ -17,10 +17,31 @@ const mission = () => {
         fetchData();
       }, [])
 
+      const handleDelete = async (missionId) => {
+        try {
+          const res = await fetch(`http://localhost:5000/api/mission/${missionId}`, {
+            method: 'DELETE',
+          });
+    
+          if (res.ok) {
+            // Hapus misi dari state setelah berhasil dihapus dari server
+            setAllMission((prevMissions) => prevMissions.filter((mission) => mission._id !== missionId));
+            console.log('Berhasil menghapus misi');
+          } else {
+            console.error('Gagal menghapus misi');
+          }
+        } catch (error) {
+          console.error('Error deleting mission:', error);
+        }
+      };
+
   return (
     <div className='w-full h-[80%] text-black flex flex-col gap-9'>
         {allMission.map((single) => (
-            <h1 className='w-[50%] bg-sky-500 font-bold text-white rounded-lg' key={single._id}>{single.mission}</h1>
+        <div className='flex'>
+            <h1 className='w-[50%] bg-sky-500 font-bold text-white rounded-lg flex' key={single._id}>{single.mission}</h1>
+            <button onClick={() => handleDelete(single._id)} className='text-white'>Delete</button>
+        </div>
         ))}
     </div>
   )
